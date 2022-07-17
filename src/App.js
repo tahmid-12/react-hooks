643a8c3f3c6013b8bc6e000ca7166c3c43c6c4c5
Md. Tahmid Chowdhury <mdtahmidchowdhury12@gmail.com>
2022-07-17 17:34:52 +0600

    Higher Order Comp in class method done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 // import Counter from "./components/Reducer-Hook/Counter/Counter";
 // import CompleCounter from "./components/Reducer-Hook/ComplexCounter/CompleCounter";
-import Counter from "./components/Reducer-Hook/Counter/Counter";
+// import Counter from "./components/Reducer-Hook/Counter/Counter";
+// import Counter from "./components/higher-order/Counter";
+// import HoverCounter from "./components/higher-order/HoverCounter";
+import Higher from "./components/higher-order/Higher";
+import styled from 'styled-components';
+
+const DIV = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+`;
 
 function App() {
   return (
-    <div>
+    <DIV>
       {/* <Counter /> */}
       {/* <CompleCounter /> */}
-      <Counter />
-    </div>
+      <Higher />
+    </DIV>
   );
 }
 
---
+++ src/components/higher-order/ClickCounter.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import UpdatedComponent from './Counter';
+
+export class ClickCounter extends Component {
+  
+  render() {
+    const { count, incrementCount } = this.props;
+    return (
+        <button onClick={incrementCount}>Clicked {count} times</button>
+    )
+  }
+}
+
+export default UpdatedComponent(ClickCounter)
